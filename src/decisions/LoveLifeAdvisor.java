f3eb3fa26c4c0f916c14358ec89ababcea9d0f86
Kaushal Patel <kapatel21@gmail.com>
2016-02-11 23:09:37 -0600

    completing

--- src/decisions/LoveLifeAdvisor.java
+++ src/decisions/LoveLifeAdvisor.java
@@ -4,15 +4,80 @@ import javax.swing.*;
 
 public class LoveLifeAdvisor
 {
+    static String result = "";
+    static boolean legal;
+    static int income;
+    static int age;
+    static int pAge;
+    static String userEntry;
+
     public static void main(String[] args)
     {
-        String result = "";
+        askAge();
 
-        boolean legal;
-        boolean difference;
-        boolean income;
+        if (age < 16)
+        {
+            result = "you are not old enough";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else legal = age >= 18;
 
+        askPAge();
 
-        JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+        if (!legal && (pAge < 18 && pAge > 15))
+        {
+            result = "Ask out good luck";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else if (pAge < 18)
+        {
+            result = "have fun in prison";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else if (Math.abs(pAge - age) <= 20)
+        {
+            result = "Ask out good luck";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else if (!legal)
+        {
+            result = "Better luck next time";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        }
+
+        askIncome();
+
+        if (income >= 200000)
+        {
+            result = "Ask out good luck";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else
+        {
+            result = "Better luck next time";
+            JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        }
+
+        System.exit(0);
+    }
+
+    private static void askIncome()
+    {
+        userEntry = JOptionPane.showInputDialog("What is your prospective date's income");
+        income = Integer.parseInt(userEntry);
+    }
+
+    private static void askPAge()
+    {
+        userEntry = JOptionPane.showInputDialog("How old is your prospective date?");
+        pAge = Integer.parseInt(userEntry);
+    }
+
+    private static void askAge()
+    {
+        userEntry = JOptionPane.showInputDialog("How old are you?");
+        age = Integer.parseInt(userEntry);
     }
 }
---
+++ src/decisions/Triangle.java
@@ -0,0 +1,72 @@
+package decisions;
+
+import javax.swing.*;
+
+public class Triangle
+{
+    public static void main(String[] args)
+    {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        String userEntry;
+        String result;
+        String triangle;
+        boolean isTriangle;
+
+
+        userEntry = JOptionPane.showInputDialog("Enter first length");
+        int a = Integer.parseInt(userEntry);
+        userEntry = JOptionPane.showInputDialog("Enter first length");
+        int b = Integer.parseInt(userEntry);
+        userEntry = JOptionPane.showInputDialog("Enter first length");
+        int c = Integer.parseInt(userEntry);
+
+        int num1 = a;
+        int num2 = b;
+        int num3 = c;
+
+        if (a > b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (b > c)
+        {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+
+        if (a > b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        isTriangle = a + b > c;
+
+
+        result = "You gave me: " + num1 + ", " + num2 + ", " + num3;
+        result+= "\n\n";
+        result+= "In order: "+ num1 + ", " + num2 + ", " + num3;
+        result+= "\n\n";
+
+        if (!isTriangle)
+            result+= "You cannot make a triangle with these sides";
+        else if (a * a + b * b == c * c)
+            result+= "You can make a right scalene triangle with these sides";
+        else if (a == b && a == c)
+            result+= "You can make an equilateral triangle with these sides";
+        else if (a != b && a != c && b != c)
+            result+= "You can make a scalene triangle with these sides";
+        else
+            result+= "You can make an isosceles triangle with these sides";
+
+        JOptionPane.showMessageDialog(null, result, "Results", JOptionPane.PLAIN_MESSAGE);
+
+        System.exit(0);
+    }
+}
