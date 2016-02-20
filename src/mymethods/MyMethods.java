d98a551d7125ddcd5eb86187030e91ae5d4b0fbc
Kaushal Patel <kapatel21@gmail.com>
2016-02-19 22:39:32 -0600

    adding mymethods and testsphere

--- src/mymethods/MyMethods.java
+++ src/mymethods/MyMethods.java
@@ -1,15 +1,21 @@
 package mymethods;
 
-import java.text.DecimalFormat;
-
 public class MyMethods
 {
     public static double sphereVolume(double radius)
     {
         return 4/3.0 * Math.PI * Math.pow(radius, 3);
     }
+
     public static double sphereArea(double radius)
     {
         return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double moPmt(double loanAmount, double apr, int years)
+    {
+        apr = apr / 12.0;
+        int n = years * 12;
+        return loanAmount * (apr + (apr / ( Math.pow(1 + apr, n) - 1) ) );
     }
 }
---
+++ src/testmymethods/TestMoPmt.java
@@ -0,0 +1,11 @@
+package testmymethods;
+
+import mymethods.MyMethods;
+
+public class TestMoPmt
+{
+    public static void main(String[] args)
+    {
+        System.out.println(MyMethods.moPmt(82900, 0.095, 15));
+    }
+}
