6e78a930d6e939308db5bef4ec5b04c4d0a23f1c
Kaushal Patel <kapatel21@gmail.com>
2016-02-28 21:20:19 -0600

    Update point

--- src/myclasses/Point.java
+++ src/myclasses/Point.java
@@ -2,5 +2,70 @@ package myclasses;
 
 public class Point
 {
+    private int x;
+    private int y;
 
+    public Point()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public void setPoint(int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+    }
+
+    public void up(int y)
+    {
+        this.y += y;
+    }
+
+    public void down(int y)
+    {
+        this.y -= y;
+    }
+
+    public void left(int x)
+    {
+        this.x -= x;
+    }
+
+    public void right(int x)
+    {
+        this.x += x;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + x + "," + y + ")";
+    }
 }
---
+++ src/testmyclasses/TestPoint.java
@@ -0,0 +1,32 @@
+package testmyclasses;
+
+import myclasses.Point;
+
+public class TestPoint
+{
+    public static void main(String[] args)
+    {
+        Point p1 =new Point();
+        Point p2 =new Point(2,1);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        p2.up(2);
+        System.out.println(p2);
+        p2.left(5);
+        System.out.println(p2);
+        p2.down(1);
+        System.out.println(p2);
+        p2.down(4);
+        System.out.println(p2);
+        p2.right(1);
+        System.out.println(p2);
+        p2.right(4);
+        System.out.println(p2);
+        p2.up(2);
+        System.out.println(p2);
+        p2.left(2);
+        System.out.println(p2);
+    }
+}
