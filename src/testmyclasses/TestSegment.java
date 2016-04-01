8069839d0797a286541ccedd2afff6e0aa02fd41
Kaushal Patel <kapatel21@gmail.com>
2016-03-31 20:43:06 -0500

    Updated segment. Added Segment2

--- src/testmyclasses/TestSegment.java
+++ src/testmyclasses/TestSegment.java
@@ -1,10 +1,34 @@
 package testmyclasses;
 
+import myclasses.Point;
+import myclasses.Segment;
+
 public class TestSegment
 {
     public static void main(String[] args)
     {
+        Point a = new Point(2, 3);
+        Point b = new Point(2, 0);
+        Point c = new Point(2, -2);
+        Point d = new Point(-3, -2);
+        Point e = new Point(-2, 0);
 
+        Segment ab = new Segment(a, b);
+        Segment ad = new Segment(a, d);
+        Segment ae = new Segment(a, e);
+        Segment be = new Segment(b, e);
+
+        System.out.println(ab);
+        System.out.println(ad);
+        System.out.println(ae);
+        System.out.println(be);
+
+        System.out.println(ab.length());
+        System.out.println(ad.length());
+        System.out.println(ae.length());
+        System.out.println(be.length());
+
+        System.out.println(ab.equals(ae));
     }
 }
 
